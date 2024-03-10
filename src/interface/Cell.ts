7bb68b1bd7709f620c9ac0c68352dcc2e5bf9497
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2024-03-10 15:56:41 -0300

    doc: Documentação da interface cell

--- src/interface/Cell.ts
+++ src/interface/Cell.ts
@@ -7,6 +7,12 @@ import Color from '../enum/Color';
  * @interface
  */
 export default interface Cell {
+    /**
+     * Cell's value, if it's equal to 0 then it is off
+     */
     value: number;
+    /**
+     * The color that should be used for rendering if the colorEnabled is equal to true on the game state property
+     */
     color: Color;
 }
