dd2a4f6bee6dcd5b4560d4272e677e75e4de48c7
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2024-03-10 18:33:21 -0300

    feat: Redirecionamento com query parameters utilizando a versão para ignorar o cache do cliente de versões antigas

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,27 @@ import Game from './engine/Game';
 import './importResources';
 import Tetris from './games/tetris/Tetris';
 
+function redirect(version: string) {
+    const params = new URL(document.location.href).searchParams;
+
+    if (params.has('v') === false || params.get('v') !== version) {
+        params.set('v', version);
+        const url = window.location.href;
+        const urlWithoutQueryParam = url.substring(0, url.indexOf('?'));
+        window.location.href = `${urlWithoutQueryParam}?${params.toString()}`;
+    }
+}
+
 export default new P5((p: P5) => {
     //Inicializando algumas variaveis
     const parentElement = document.querySelector(PARENT_SELECTOR);
 
     //Info versão
+    const { version } = require('./../package.json');
     const versionElement = document.querySelector('#version');
-    versionElement.innerHTML = `version: ${require('./../package.json').version}`;
+    versionElement.innerHTML = `version: ${version}`;
+
+    redirect(version);
 
     const body: GameBody = new GameBody({
         parent: parentElement,
